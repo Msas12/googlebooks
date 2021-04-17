3e559695b84008e00f5e1eb2c731fcaff53cc4f6
Mike Sasiela <m5455y@gmail.com>
2021-04-17 12:13:18 -0500

    final

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
+// eslint-disable-next-line
 export default {
-    searchBooks : function(book){
-        return axios.get("https://www.googleapis.com/books/v1/volumes?q="+book);        
-    },
-    saveBook : function(bookData){
-        return axios.post("/api/books", bookData);
-    },
-    findBooks : function(){
-        return axios.get("/api/books");
-    },
-    deleteBook : function(id){
-        return axios.delete("/api/books/"+id)
-    }
-
-}
+  searchBooks: function (book) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
+  },
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+  findBooks: function () {
+    return axios.get("/api/books");
+  },
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+};
